7d25da5dc138f3595b4dbb4b4a162a71db36e88e
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-02-17 01:21:32 +0100

    [Python] Change test to testing extra requirement name

--- python/setup.py
+++ python/setup.py
@@ -26,5 +26,5 @@ setup(
     packages=["uonet_request_signer"],
     package_dir={"uonet_request_signer": "uonet-request-signer"},
     install_requires=["pyopenssl"],
-    extras_require={"test": ["pytest"]},
+    extras_require={"testing": ["pytest"]},
 )
