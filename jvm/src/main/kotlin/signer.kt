6d7aebf0e9ed6f812bb21d61bc7d8bacff52932d
Mikołaj Pich <m.pich@outlook.com>
2019-10-29 11:24:04 +0100

    [jvm] Add methods to sign content with only private key

--- jvm/src/main/kotlin/signer.kt
+++ jvm/src/main/kotlin/signer.kt
@@ -2,9 +2,11 @@ package io.github.wulkanowy.signer
 
 import org.apache.commons.codec.binary.Base64
 import java.io.ByteArrayInputStream
+import java.security.KeyFactory
 import java.security.KeyStore
 import java.security.PrivateKey
 import java.security.Signature
+import java.security.spec.PKCS8EncodedKeySpec
 
 fun signContent(password: String, certificate: String?, content: String): String {
     val keystore = KeyStore.getInstance("pkcs12").apply {
@@ -15,4 +17,27 @@ fun signContent(password: String, certificate: String?, content: String): String
         update(content.toByteArray())
     }
     return Base64.encodeBase64String(signature.sign())
+}
+
+fun signContent(privateKey: String, content: String): String {
+    val key = PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey)).let {
+        KeyFactory.getInstance("RSA").generatePrivate(it)
+    }
+    val signature = Signature.getInstance("SHA1WithRSA").apply {
+        initSign(key)
+        update(content.toByteArray())
+    }
+    return Base64.encodeBase64String(signature.sign())
+}
+
+fun getPrivateKeyFromCert(password: String, certificate: String): String {
+    val keystore = KeyStore.getInstance("pkcs12").apply {
+        load(ByteArrayInputStream(Base64.decodeBase64(certificate)), password.toCharArray())
+    }
+    val keyFactory = KeyFactory.getInstance("RSA")
+    val keySpec = keyFactory.getKeySpec(
+        keystore.getKey("LoginCert", password.toCharArray()) as PrivateKey,
+        PKCS8EncodedKeySpec::class.java
+    )
+    return Base64.encodeBase64String(keySpec.encoded)
 }
