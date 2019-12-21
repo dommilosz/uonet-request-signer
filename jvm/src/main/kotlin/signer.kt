36c43658092951080e03141f06b7ad2d6f600cd9
Mikołaj Pich <m.pich@outlook.com>
2019-12-21 23:09:00 +0100

    [jvm] Replace commons-codec with migbase64 lib

--- jvm/src/main/kotlin/signer.kt
+++ jvm/src/main/kotlin/signer.kt
@@ -1,6 +1,6 @@
 package io.github.wulkanowy.signer
 
-import org.apache.commons.codec.binary.Base64
+import com.migcomponents.migbase64.Base64
 import java.io.ByteArrayInputStream
 import java.security.KeyFactory
 import java.security.KeyStore
@@ -10,34 +10,34 @@ import java.security.spec.PKCS8EncodedKeySpec
 
 fun signContent(password: String, certificate: String?, content: String): String {
     val keystore = KeyStore.getInstance("pkcs12").apply {
-        load(ByteArrayInputStream(Base64.decodeBase64(certificate)), password.toCharArray())
+        load(ByteArrayInputStream(Base64.decode(certificate)), password.toCharArray())
     }
     val signature = Signature.getInstance("SHA1WithRSA").apply {
         initSign(keystore.getKey("LoginCert", password.toCharArray()) as PrivateKey)
         update(content.toByteArray())
     }
-    return Base64.encodeBase64String(signature.sign())
+    return Base64.encodeToString(signature.sign(), false)
 }
 
 fun signContent(privateKey: String, content: String): String {
-    val key = PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey)).let {
+    val key = PKCS8EncodedKeySpec(Base64.decode(privateKey)).let {
         KeyFactory.getInstance("RSA").generatePrivate(it)
     }
     val signature = Signature.getInstance("SHA1WithRSA").apply {
         initSign(key)
         update(content.toByteArray())
     }
-    return Base64.encodeBase64String(signature.sign())
+    return Base64.encodeToString(signature.sign(), false)
 }
 
 fun getPrivateKeyFromCert(password: String, certificate: String): String {
     val keystore = KeyStore.getInstance("pkcs12").apply {
-        load(ByteArrayInputStream(Base64.decodeBase64(certificate)), password.toCharArray())
+        load(ByteArrayInputStream(Base64.decode(certificate)), password.toCharArray())
     }
     val keyFactory = KeyFactory.getInstance("RSA")
     val keySpec = keyFactory.getKeySpec(
         keystore.getKey("LoginCert", password.toCharArray()) as PrivateKey,
         PKCS8EncodedKeySpec::class.java
     )
-    return Base64.encodeBase64String(keySpec.encoded)
+    return Base64.encodeToString(keySpec.encoded, false)
 }
